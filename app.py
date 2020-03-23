88c76b72804151ebaa75730dc635ad7a0467b084
Mohan Sai <31368099+MohanSai1997@users.noreply.github.com>
2020-03-23 14:54:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! How are You"
+    return "Hello World! From Flux"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
