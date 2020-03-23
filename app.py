006fdffb6a3a23eb53d70c27cc204ce9b628b986
Mohansai <tmohansk@gmail.com>
2020-03-23 13:53:30 +0530

    ENV variable is added

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Now It's working"
+    return os.environ.get("NAME")
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
