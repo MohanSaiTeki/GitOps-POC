408a84f5a752f55adab7f9d7dc32df7b2ae51ba7
siddhartha <siddhartha.banti@NODE1188.gspann.local>
2020-03-23 14:43:54 +0530

    Updating Files

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return os.environ.get("NAME")
+    return "Hello World! How are You"
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
